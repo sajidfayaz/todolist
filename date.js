b8715cf97f5fe7fc1dfd421663ccda3ee88fe3ae
Sajid Fayaz <50862864+sajidfayaz@users.noreply.github.com>
2022-01-08 17:05:08 +0530

    Add comments

--- date.js
+++ date.js
@@ -1,3 +1,7 @@
+//This was intended to be the title of every page
+// but I dropped it later as I put the title of each page same as the name it was initiated with/created with.
+// you can still use it if you want
+
 exports.getDate = () => {
 
     const today = new Date();
